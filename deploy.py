dbb175d85de43276676e6b7d69eaf6f5e34306b2
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-22 02:09:06 +0700

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import json
 
-def auto_deploy(instance_path: str, master_api_key: str):
+def auto_deploy(instance_path: str, master_api_key: str, port: int):
     config_path = os.path.join(instance_path, "config.json")
     with open(config_path) as f:
         config = json.load(f)
@@ -11,6 +11,6 @@ def auto_deploy(instance_path: str, master_api_key: str):
         raise PermissionError("Master API Key mismatch!")
     
     subprocess.run(["pip", "install", "-r", "requirements.txt"], cwd=instance_path)
-    subprocess.Popen(["uvicorn", "run_api:app", "--host", "0.0.0.0", "--port", "8000"], cwd=instance_path)
+    subprocess.Popen(["uvicorn", "run_api:app", "--host", "0.0.0.0", f"--port={port}"], cwd=instance_path)
     
-    print(f"Public deployment complete: {instance_path}")
+    print(f"Deployment complete: {instance_path} on port {port}")

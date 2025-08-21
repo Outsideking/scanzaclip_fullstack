158481e9a01c4df099531d9b974b0f157804c258
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-22 01:54:28 +0700

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,7 +1,8 @@
 SCANZACLIP_MODULES = {
     "vision": {"version": "1.0", "description": "Vision AI module"},
     "ai_core": {"version": "2.1", "description": "Core AI processing"},
-    "speech": {"version": "1.0", "description": "Speech recognition module"}
+    "speech": {"version": "1.0", "description": "Speech recognition module"},
+    "nlp": {"version": "1.0", "description": "Natural Language Processing module"}
 }
 
 def get_module(name):
